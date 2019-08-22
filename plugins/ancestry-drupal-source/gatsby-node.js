4ce1b67de512c84f7a4d3ca2d33f93b9d2fa3839
anithegregorian <anithegregorian@gmail.com>
2019-08-22 22:16:27 +0530

    - fixed Gatsby compile errors

--- plugins/ancestry-drupal-source/gatsby-node.js
+++ plugins/ancestry-drupal-source/gatsby-node.js
@@ -2,15 +2,13 @@ const axios = require('axios');
 const crypto = require('crypto');
 
 
-
-
 exports.sourceNodes = async ({actions}) => {
-  const {createNode} = actions;
-  const result = await axios({
-                    method: "POST",
-                    url: "http://dev-ancestry.pantheonsite.io/graphql",
-                    data: {
-                        query: `
+	const {createNode} = actions;
+	const result = await axios({
+		method: "POST",
+		url: "http://dev-ancestry.pantheonsite.io/graphql",
+		data: {
+			query: `
 											query BlogWithAuthorAndTags($nid: String) {
 						  node(nid: $nid) {
 							nid
@@ -44,26 +42,28 @@ exports.sourceNodes = async ({actions}) => {
 						  }
 						}		
                         `
-                    }
-                });
-  for (const ancestrynode of result.data.data.node) {
-    await createNode({
-      children: [],
-      id: ancestrynode.nid.toString(),
-      title: ancestrynode.title,
-	  body:{value: ancestrynode.body.value,
-			summary: ancestrynode.body.summary,
-			format: ancestrynode.body.format},
-	  field_featured:{value:ancestrynode.field_featured.value},
-	  field_category:{tid:ancestrynode.field_category.tid},
-      parent: null,
-      internal: {
-        type: 'ancestrynode',
-        contentDigest: crypto
-          .createHash(`md5`)
-          .update(JSON.stringify(ancestrynode))
-          .digest(`hex`),
-      },
-    });
-  }
+		}
+	});
+	for (const ancestrynode of result.data.data.node) {
+		await createNode({
+			children: [],
+			id: ancestrynode.nid.toString(),
+			title: ancestrynode.title,
+			body: {
+				value: ancestrynode.body.value,
+				summary: ancestrynode.body.summary,
+				format: ancestrynode.body.format
+			},
+			field_featured: {value: ancestrynode.field_featured.value},
+			field_category: {tid: ancestrynode.field_category.tid},
+			parent: null,
+			internal: {
+				type: 'ancestrynode',
+				contentDigest: crypto
+					.createHash(`md5`)
+					.update(JSON.stringify(ancestrynode))
+					.digest(`hex`),
+			},
+		});
+	}
 };
