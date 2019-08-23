41049415cbc2501168ae2ee0e8f6cb6cf7c6c56c
vasi <vasi@exemplifai.io>
2019-08-23 15:32:29 +0530

    hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,14 +8,16 @@ export default () => (
 
 			<div className="view view-home-blogs-hero-section view-id-home_blogs_hero_section view-display-id-block view-dom-id-fed2853a10e673ce5bfd2046bf8a4790">
 
-				<div className="view-content row">
-					<div className="views-row views-row-1 node-90">
+				 
+					<div class="view-content row in-view">
+						<div class="views-row views-row-1 node-90">
 
-						<h2><a href="javascript:void(0)" rel="external">Understanding your new ethnicity estimate</a></h2>
+						<h2><a href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Understanding your new ethnicity estimate</a></h2>
 						<p>We’re always looking for new ways to help support our customers on their journeys of personal discovery. Often that means updating our products and services to take advantage of the most advanced science and technology.</p>
 
-						<a className="btn btn-outline-ancestry btn-lg" href="javascript:void(0)" rel="external">Read This Post</a></div>
-				</div>
+						<a class="btn btn-outline-ancestry btn-lg" href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Read This Post</a>    </div>
+						</div>
+				 
 
 
 			</div>
