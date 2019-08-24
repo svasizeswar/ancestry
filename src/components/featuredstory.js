3f7994ec4a327a469063099435a566e20bbcad6c
anithegregorian <anithegregorian@gmail.com>
2019-08-25 01:19:12 +0530

    - latest blogs component

--- src/components/featuredstory.js
+++ src/components/featuredstory.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 //import { useStaticQuery, graphql } from "gatsby"
 
 import axios from "axios"
 
 
 class FeaturedStory extends Component {
-	 state = {
-        title1: " ",
+	state = {
+		title1: " ",
 		title2: " "
-    }
-	 componentDidMount() {	 
+	};
+
+	componentDidMount() {
 		axios({
-		  url: "http://devel.ravendevelopers.com/ancestry/graphql",
-		  method: 'post',
-		  data: {
-			query: `
+			url: "http://devel.ravendevelopers.com/ancestry/graphql",
+			method: 'post',
+			data: {
+				query: `
 					query {
 					  nodeById(id: "88") {
 						entityId
@@ -25,15 +26,15 @@ class FeaturedStory extends Component {
 					}	`
 			}
 		}).then((result) => {
-			console.log(result)
- 		  	const blocktitle = result.data.data.nodeById.title;
-			this.setState({ title1: blocktitle })
+			//console.log(result);
+			const blocktitle = result.data.data.nodeById.title;
+			this.setState({title1: blocktitle})
 		});
 		axios({
-		  url: "http://devel.ravendevelopers.com/ancestry/graphql",
-		  method: 'post',
-		  data: {
-			query: `
+			url: "http://devel.ravendevelopers.com/ancestry/graphql",
+			method: 'post',
+			data: {
+				query: `
 					query {
 					  nodeById(id: "89") {
 						entityId
@@ -44,45 +45,45 @@ class FeaturedStory extends Component {
 					}	`
 			}
 		}).then((result) => {
-			console.log(result)
- 		  	const blocktitle = result.data.data.nodeById.title;
-			this.setState({ title2: blocktitle })
+			//console.log(result);
+			const blocktitle = result.data.data.nodeById.title;
+			this.setState({title2: blocktitle})
 		});
-	
 
-	 }
 
- render() {
-        return (
-<div class="container views-home-featured-story-block-container block-views " id="home-featured-stories">
-   <div class="view view-home-featured-story view-id-home_featured_story view-display-id-block view-article-wrapper view-dom-id-e3ee66df1d39f6396ad06479b404c440">
-      <div class="view-content">
-         <ul class="article-list article-list-blue content-overlay list-unstyled row">
-            <li class="views-row views-row-1 views-row-odd views-row-first col-24 col-md-12 node-88 in-view">
-               <div class="encapsulated">
-                  <img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/Sarah_Booth_1.jpg?itok=kqqzWfKU" width="600" height="600" alt="" class="medium img-fluid"></img>
-                  <div class="story-content">
-                     <h3><a href="/content/sarah-booth-four-year-ancestry-member">{this.state.title2}</a></h3>
-                  </div>
-               </div>
-            </li>
-            <li class="views-row views-row-2 views-row-even views-row-last col-24 col-md-12 node-89 in-view">
-               <div class="encapsulated">
-                  <img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/jason_jones_1.jpg?itok=8z--j9_w" width="600" height="600" alt="" class="medium img-fluid"></img>
-                  <div class="story-content">
-                     <h3><a href="/content/jason-jones-ancestral-voyage-trip-great-britain-and-ireland">{this.state.title1}</a></h3>
-                  </div>
-               </div>
-            </li>
-         </ul>
-      </div>
-      <footer class="view-footer">
-         <p class="h4">We’ve pioneered and defined this category, developing new innovations and technologies that have reinvented how people make family history discoveries. And these discoveries can give everyone a greater sense of identity, relatedness, and their place in the world.</p>
-         <a class="btn btn-outline-ancestry btn-blue btn-lg" href="#">View Our Full Story</a>			
-      </footer>
-   </div>
-</div> )
- }
+	}
+
+	render() {
+		return (
+			<div class="container views-home-featured-story-block-container block-views " id="home-featured-stories">
+				<div class="view view-home-featured-story view-id-home_featured_story view-display-id-block view-article-wrapper view-dom-id-e3ee66df1d39f6396ad06479b404c440">
+					<div class="view-content">
+						<ul class="article-list article-list-blue content-overlay list-unstyled row">
+							<li class="views-row views-row-1 views-row-odd views-row-first col-24 col-md-12 node-88 in-view">
+								<div class="encapsulated">
+									<img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/Sarah_Booth_1.jpg?itok=kqqzWfKU" width="600" height="600" alt="" class="medium img-fluid"></img>
+									<div class="story-content">
+										<h3><a href="/content/sarah-booth-four-year-ancestry-member">{this.state.title2}</a></h3>
+									</div>
+								</div>
+							</li>
+							<li class="views-row views-row-2 views-row-even views-row-last col-24 col-md-12 node-89 in-view">
+								<div class="encapsulated">
+									<img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/jason_jones_1.jpg?itok=8z--j9_w" width="600" height="600" alt="" class="medium img-fluid"></img>
+									<div class="story-content">
+										<h3><a href="/content/jason-jones-ancestral-voyage-trip-great-britain-and-ireland">{this.state.title1}</a></h3>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<footer class="view-footer">
+						<p class="h4">We’ve pioneered and defined this category, developing new innovations and technologies that have reinvented how people make family history discoveries. And these discoveries can give everyone a greater sense of identity, relatedness, and their place in the world.</p>
+						<a class="btn btn-outline-ancestry btn-blue btn-lg" href="#">View Our Full Story</a>
+					</footer>
+				</div>
+			</div>)
+	}
 }
 
 export default FeaturedStory
--- src/components/latestblog.js
+++ src/components/latestblog.js
@@ -1,13 +1,20 @@
 import React, {Component} from "react"
+import lodash from "lodash"
+import axios from "axios";
 //import axios from "axios"
 //const axios = require('axios');
 //import { graphql } from "gatsby"
 
 class HomeFeaturedBlogArticles extends Component {
 
-	state = {
-		blogs: []
-	};
+	// state = {
+	// 	blogs: {}
+	// };
+
+  constructor(props) {
+    super(props);
+    this.state = {blogs: {}};
+  }
 
 	static async getBlogArticles() {
 		const axios = require('axios');
@@ -49,57 +56,106 @@ fragment blogFragment on NodeBlog {
 			}
 		});
 
-		return result;
+		//console.log(result.data.data);
+
+		return result.data.data;
 	}
 
 	componentDidMount() {
-		this.setState({blogs: HomeFeaturedBlogArticles.getBlogArticles()});
+		//this.setState({blogs: HomeFeaturedBlogArticles.getBlogArticles()});
+
+		axios({
+			url: "http://devel.ravendevelopers.com/ancestry/graphql",
+			method: 'post',
+			data: {
+				query: `
+query {
+  nodeOne: nodeById(id: "86") {
+    ... blogFragment
+  }
+  nodeTwo: nodeById(id: "81") {
+    ... blogFragment
+  }
+}
+
+fragment blogFragment on NodeBlog {
+  title
+  body{
+    processed
+  }
+  fieldBlogPostUrl
+  fieldBlogPostAuthor
+  fieldFeatured
+  fieldCategory {
+    targetId
+  }
+  fieldBlogPhoto {
+    targetId
+    alt
+    title
+    width
+    height
+    url
+  }
+}
+				`
+			}
+		}).then((result) => {
+			const data = result.data.data;
+			this.setState({blogs: data});
+		});
+
 	};
 
 	render() {
-		console.log(this.state.blogs);
+		const elements = [];
 
-		// for (const blog of this.state.blogs) {
-		// 	console.log(blog);
-		// }
+		for (const key in this.state.blogs) {
+			// check also if property is not inherited from prototype
+			if (this.state.blogs.hasOwnProperty(key)) {
+				const value = this.state.blogs[key];
+				switch (value.fieldBlogPhoto.targetId){
+					case 55:
+						value.featuredPhoto = 'http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/FULL-Tuscany.jpg?itok=Hf5ak1yg';
+						break;
+					case 57:
+						value.featuredPhoto = 'http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/adam-zvanovec-34686_0.jpg?itok=a7OWxa_Y';
+						break;
+				}
+				elements.push(value);
+			}
+		}
 
 		return (
 			<div className="container views-home-featured-blogs-block-container block-views " id="">
 				<div className="view view-home-featured-blogs view-id-home_featured_blogs view-display-id-block archive-blogs view-article-wrapper mt-nth3 view-dom-id-33daa44a3d3dd8d90398ce3c875ff549">
 					<div className="view-content">
 						<ul className="article-list content-overlay list-unstyled row">
-							<li className="views-row views-row-1 views-row-odd views-row-first col-24 col-md-12 node-86 in-view">
-								<article>
-									<img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/FULL-Tuscany.jpg?itok=Hf5ak1yg" width="600" height="600" alt="" className="medium img-fluid"></img>
-									<div className="blog-content">
-										<header className="article-title trim-two-liner">
-											<h3><a href="/content/insights-culture-and-events-ancestry">Insights on Culture and Events at Ancestry</a></h3>
-										</header>
-										<section>
-											<p>Ancestry recently announced the creation of a Tech and Product User Group in Utah with the goal of encouraging collaboration between companies across...</p>
-										</section>
-										<footer>
-											<p className="meta-info"><a className="comments" href="/content/insights-culture-and-events-ancestry#disqus_thread" rel="external" target="_blank">0 Comments</a>&nbsp;&nbsp;<a className="read-more" href="/content/insights-culture-and-events-ancestry" rel="external" target="_blank"><strong>Read More&nbsp;&nbsp;&gt;</strong></a></p>
-										</footer>
-									</div>
-								</article>
-							</li>
-							<li className="views-row views-row-2 views-row-even views-row-last col-24 col-md-12 node-81 in-view">
-								<article>
-									<img typeof="foaf:Image" src="http://dev-ancestry.pantheonsite.io/sites/default/files/styles/medium/public/adam-zvanovec-34686_0.jpg?itok=a7OWxa_Y" width="600" height="600" alt="" className="medium img-fluid"></img>
-									<div className="blog-content">
-										<header className="article-title trim-two-liner">
-											<h3><a href="/content/ancestry-women-technology-year-review">Ancestry Women in Technology year in review</a></h3>
-										</header>
-										<section>
-											<p>Ancestry recently announced the creation of a Tech and Product User Group in Utah with the goal of encouraging collaboration between companies across...</p>
-										</section>
-										<footer>
-											<p className="meta-info"><a className="comments" href="/content/ancestry-women-technology-year-review#disqus_thread" rel="external" target="_blank">0 Comments</a>&nbsp;&nbsp;<a className="read-more" href="/content/ancestry-women-technology-year-review" rel="external" target="_blank"><strong>Read More&nbsp;&nbsp;&gt;</strong></a></p>
-										</footer>
-									</div>
-								</article>
-							</li>
+
+							{elements.map((value, index) => {
+								return (
+									<li className="views-row views-row-{index} views-row-odd views-row-first col-24 col-md-12 node-86 in-view" key={index}>
+										<article>
+											<img typeof="foaf:Image" src={value.featuredPhoto} width="600" height="600" alt="" className="medium img-fluid"></img>
+											<div className="blog-content">
+												<header className="article-title trim-two-liner">
+													<h3><a href="/">{value.title}</a></h3>
+												</header>
+												<section>
+													{value.body.processed.replace(/(<([^>]+)>)/ig,"")}
+												</section>
+												<footer>
+													<p className="meta-info">
+														<a className="comments" href="javascript:void(0)" rel="external">0 Comments</a>&nbsp;&nbsp;
+														<a className="read-more" href="javascript:void(0)" rel="external"><strong>Read More&nbsp;&nbsp;&gt;</strong></a>
+													</p>
+												</footer>
+											</div>
+										</article>
+									</li>
+								);
+							})}
+
 						</ul>
 					</div>
 					<footer className="view-footer">
