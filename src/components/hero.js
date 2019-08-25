2cab2dfbc39ef733b5bc813f5cd8adcebc4c13f4
anithegregorian <anithegregorian@gmail.com>
2019-08-25 17:31:07 +0530

    - modified hero component (more dynamic)

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,27 +1,40 @@
-import React from "react"
+import React, {Component} from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-export default () => (
-	<div className="jumbotron">
+class HomeJumbotron extends Component {
 
-		<div className='container-default views-home-blogs-hero-section-block-container block-views jumbotron-generic one-cols-jumbotron' id='home-jumbotron'>
+  constructor(props) {
+    super(props);
+  }
 
-			<div className="view view-home-blogs-hero-section view-id-home_blogs_hero_section view-display-id-block view-dom-id-fed2853a10e673ce5bfd2046bf8a4790">
+	render() {
+		const divStyle = {
+			backgroundImage: 'url(http://dev-ancestry.pantheonsite.io/sites/default/files/styles/jumbotron_one_column/public/sarah_booth.jpg?itok=q2PDEcM7)',
+		};
+		return(
+			<div className="jumbotron">
 
-				 
-					<div class="view-content row in-view">
-						<div class="views-row views-row-1 node-90">
+				<div className='container-default views-home-blogs-hero-section-block-container block-views jumbotron-generic one-cols-jumbotron' id='home-jumbotron' style={divStyle}>
 
-						<h2><a href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Understanding your new ethnicity estimate</a></h2>
-						<p>We’re always looking for new ways to help support our customers on their journeys of personal discovery. Often that means updating our products and services to take advantage of the most advanced science and technology.</p>
+					<div className="view view-home-blogs-hero-section view-id-home_blogs_hero_section view-display-id-block view-dom-id-fed2853a10e673ce5bfd2046bf8a4790">
 
-						<a class="btn btn-outline-ancestry btn-lg" href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Read This Post</a>    </div>
+						<div className="view-content row in-view">
+							<div className="views-row views-row-1 node-90">
+
+								<h2><a href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Understanding your new ethnicity estimate</a></h2>
+								<p>We’re always looking for new ways to help support our customers on their journeys of personal discovery. Often that means updating our products and services to take advantage of the most advanced science and technology.</p>
+
+								<a className="btn btn-outline-ancestry btn-lg" href="/content/understanding-your-new-ethnicity-estimate" rel="external" target="_blank">Read This Post</a></div>
 						</div>
-				 
 
 
+					</div>
+
+				</div>
 			</div>
+		)
+	}
 
-		</div>
-	</div>
-)
+}
+
+export default HomeJumbotron;
