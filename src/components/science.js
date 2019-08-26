9bdbfa08579acf5aabaee89ad2df1aa379d08109
anithegregorian <anithegregorian@gmail.com>
2019-08-26 16:35:01 +0530

    - updated science block with blockQueryById

--- src/components/science.js
+++ src/components/science.js
@@ -1,25 +1,52 @@
 import React, {Component} from "react"
-import HomeFeaturedBlogArticles from "./latestblog";
+import axios from "axios";
 
 class HomeScienceBlock extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {blogs: {}};
+		this.state = {blocks: {}};
 	}
 
 	componentDidMount() {
+		axios({
+			url: "http://devel.ravendevelopers.com/ancestry/graphql",
+			method: 'post',
+			data: {
+				query: `
+query {
+blockContentById(id: "5"){
+    entityLabel
+    entityBundle
+    ...body
+  }
+}
 
+fragment body on BlockContentBasic{
+  body{
+    value
+  }
+}
+				`
+			}
+		}).then((result) => {
+			const data = result.data.data;
+			this.setState({blocks: data});
+		});
 	}
 
 	render(){
+		let element = this.state.blocks;
+
+		let blockContent = element.blockContentById.body.value.replace(/(<([^>]+)>)/ig,"");
+
 		return (
 			<div className="container-default block-5-container block-block " id="ancestry-science-tech">
 				<div className="block-content">
 					<div className="container">
 						<div className="row justify-content-center">
 							<div className="col-24 col-lg-18 col-md-20">
-								<h3>We’re a science and technology company with a very human mission</h3>
+								<h3>{blockContent}</h3>
 							</div>
 						</div>
 					</div>
@@ -31,17 +58,3 @@ class HomeScienceBlock extends Component {
 }
 
 export default HomeScienceBlock;
-
-// export default () => (
-// 	<div className="container-default block-5-container block-block " id="ancestry-science-tech">
-// 		<div className="block-content">
-// 			<div className="container">
-// 				<div className="row justify-content-center">
-// 					<div className="col-24 col-lg-18 col-md-20">
-// 						<h3>We’re a science and technology company with a very human mission</h3>
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</div>
-// )
